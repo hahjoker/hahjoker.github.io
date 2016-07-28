ce8ebe2de26f1057e7a9c4879aa6d87dc58d09f1
Pranav Chaudhari <awesomesauceproduction@gmail.com>
2016-07-28 09:51:21 -0400

    qweqw

--- assets/js/another.js
+++ assets/js/another.js
@@ -17,8 +17,12 @@ $(document).ready(function(){
     $("#card").click(function(){
       var sourceLength = sMH.length;
       var randomNumber= Math.floor(Math.random()*sourceLength);
-			var COMEON=$('#ayy');
+			var flip = $("#card").data("flip-model");
+			var toomuch='';
+			if(flip.isFlipped){
+				var COMEON=$('#ayy');
 			var toomuch=sMH[randomNumber].quote;
+			}
 			COMEON.html('');
       COMEON.append('<p>'+toomuch+'</p>');
     });

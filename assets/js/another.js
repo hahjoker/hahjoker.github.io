59b7c257e781e45ea22cae9174dd33a4e6064889
Pranav Chaudhari <awesomesauceproduction@gmail.com>
2016-07-27 21:43:32 -0400

    the fix

---
+++ assets/js/another.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+		var sMH=[
+		  {
+	    	quote:"I'm published",
+	    },
+			{
+				quote: "I thought you were my boy"
+			}
+			{
+				quote: "too lazy to come up with more of these"
+			}
+	];
+    $("#card").flip({
+      axis: 'y',
+      trigger: 'click'
+    });
+    $("#card").click(function(){
+      var sourceLength = sMH.length;
+      var randomNumber= Math.floor(Math.random()*sourceLength);
+			var COMEON=$('#ayy');
+			var toomuch=sMH[randomNumber].quote;
+			COMEON.html('');
+      COMEON.append('<p>'+toomuch+'</p>');
+    });
+});
